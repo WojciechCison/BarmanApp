a7d1ba41fd6b7110157b3a54dd3f9d3abd1719f5
Piotr Talik <piotrektalik1@gmail.com>
2023-01-29 21:53:33 +0100

    FE Coctails / Modals/ Delete

--- frontapp/src/components/ingredients.js
+++ frontapp/src/components/ingredients.js
@@ -28,6 +28,8 @@ import Modal from '@mui/material/Modal';
 import AddIcon from '@mui/icons-material/Add';
 import TextField from '@mui/material/TextField';
 import RemoveIcon from '@mui/icons-material/Remove';
+import TablePagination from '@mui/material/TablePagination';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const brake = { margin: '80px 30px' }
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
@@ -99,6 +101,9 @@ const theme = createTheme({
         main: '#80461B',
         contrastText: '#fff',
       },
+      black:{
+        main: '#000'
+      }
     },
   });
 
@@ -106,53 +111,98 @@ export default class Ingredients extends React.Component{
   constructor(props){
     super(props);
     this.state = { open: false };
+    this.state = { checked: false };
+    this.handleChange = this.handleChange.bind(this);
   }
   
   handleOpen = () => {
     this.setState({ open: true });
   };
 
-  handleClose = () => {
+  handleChange(event) {
+    this.setState({ checked: event.target.checked });
+    console.log(event)
+    }
+
+  handleClose = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const addNewIngridientData = {
+      name: data.get("name"),
+      unit: data.get("unit")
+    }
+    this.CreateIngridient(addNewIngridientData);
     this.setState({ open: false });
   };
  async ingridientsRequest() {
     const token = getToken();
     if(token){
-        const data = await axios.get(`http://localhost:5233/ingridients/${token}` )
+        const data = await axios.get(`http://localhost:5555/ingridients/${token}` )
         .then(response => {
-            console.log("data");
+            
             return response.data 
+            
         })
         .catch(error => {
             console.log(error);
         })
         this.setState({data: data})
         return data
+       
     }
   }
+   DeleteIngridient = (id)  => {
+    const token = getToken();
+    console.log(token)
+    const data = axios.delete(`http://localhost:5555/ingridients/${id}`,{data:`"${token}"`} )
+    .then(response => {
+       this.ingridientsRequest();
+        return response.data 
+    })
+    .catch(error => {
+        console.log(error);
+    })
+};
+
+CreateIngridient  = (Ingridientdata) => {
+  const token = getToken();
+  Ingridientdata.token = token;
+  console.log(Ingridientdata)
+  axios.post("http://localhost:5555/ingridients", Ingridientdata)
+  
+    .then(response => {
+      this.ingridientsRequest();
+      console.log(response)
+   })
+  .catch(error => {
+      console.log(error);
+  })
+}
   componentDidMount(){
     this.ingridientsRequest();
   }
-  
+
+
 
   render() {
+    
     const ingridients = this.state;
     return (
       <div>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: '100vh' }} >
         <CssBaseline />
         <Grid
           item
           xs={false}
-          sm={4}
+          sm={8}
           md={3}
           sx={{
             backgroundImage: 'url(https://images.unsplash.com/photo-1629274535685-846d95e04095?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=782&q=80)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+                        backgroundPosition: 'center',
+                        height: '100%'
           }}
         />
 
@@ -209,20 +259,20 @@ export default class Ingredients extends React.Component{
                 <Button variant="outlined" style={{ height: 60, width: 230, marginBottom: 0,  marginTop: 80, marginLeft: 30 }} color="primary" onClick={this.handleOpen} startIcon={<AddIcon /> }> 
                   Add New Ingridient
                 </Button>
-                <Modal
+        <Modal
           open={this.state.open}
           onClose={this.handleClose}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box sx={style}>
+          <Box sx={style} >
             <Typography id="modal-modal-title" variant="h6" component="h2">
               Add new Ingridient
             </Typography>
             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
               Fill in all required informations
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Box noValidate component="form" onSubmit={this.handleClose} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -239,19 +289,9 @@ export default class Ingredients extends React.Component{
                 <TextField
                   required
                   fullWidth
-                  id="ingridients"
-                  label="Ingridients"
-                  name="ingridients"
-                
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="Unit"
+                  id="unit"
                   label="Unit"
-                  name="Unit"
+                  name="unit"
                 
                 />
               </Grid>
@@ -263,7 +303,7 @@ export default class Ingredients extends React.Component{
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              onSubmit = {handleSubmit}
+              onSubmit={this.handleClose}
             >
               Create New Ingridient
             </Button>
@@ -271,9 +311,7 @@ export default class Ingredients extends React.Component{
           </Box>
             </Modal>
 
-        <Button variant="outlined" style={{ height: 60, width: 230, marginBottom: 0,  marginTop: 80, marginLeft: 30 }} color="primary" startIcon={<RemoveIcon /> }> 
-                  Delete Ingridient
-                </Button>
+
         
                 </div>
 
@@ -281,17 +319,16 @@ export default class Ingredients extends React.Component{
                 
                 </Box>
 
-                <Grid style={{margin: '20px 30px'}} sx={{
-        width: 1380,
-        maxWidth: '100%',
+                <Grid style={{margin: '20px 0px 0px 30px'}} sx={{
+        width: 1380
       }}>
-                        <TableContainer >
-      <Table sx={{ minWidth: 650 }} aria-label="customized table">
+      <TableContainer sx={{maxHeight:"60vh", overflowY:"auto"}} >
+      <Table sx={{ minWidth: 650,}} aria-label="customized table" stickyHeader >
         <TableHead>
-          <StyledTableRow >
+          <StyledTableRow>
             <StyledTableCell>Id</StyledTableCell>
             <StyledTableCell>Ingridient name</StyledTableCell>
-            
+            <StyledTableCell>Quantity</StyledTableCell>
             <StyledTableCell align='right'>Unit</StyledTableCell>
           </StyledTableRow >
         </TableHead>
@@ -301,21 +338,22 @@ export default class Ingredients extends React.Component{
               key={el.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-              <Checkbox {...label}  />
+              <TableCell component="th" scope="row" >
                 {el.id}
+                <Button onClick={() => this.DeleteIngridient(el.id)}  startIcon={<DeleteIcon color="action" />} >  </Button>
                 </TableCell>
               <TableCell component="th" scope="row">
                 {el.name}
               </TableCell>
-              <TableCell align="right">
-              
-              <div>{el.coctailIngridients.map((test) => {
-            return(<div>{test.name+" "+test.dose+"x"+test.unit}</div>)
-          })}</div>
-              
-              
+              <TableCell component="th" scope="row">
+                {el.quantity}
               </TableCell>
+              <TableCell align="right" component="th" scope="row">
+                {el.unit}
+              </TableCell>
+              
+    
+              
 
             </StyledTableRow >
           ))}
